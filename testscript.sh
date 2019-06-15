82ef383a80466b23b9f789593c08e447ce79fdfd
sgarrapupiz <51853489+sgarrapupiz@users.noreply.github.com>
2019-06-15 15:12:45 +0200

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "ceppa!"
+echo "I don't really know, but maybe yes"
+echo "this was run automagically"
